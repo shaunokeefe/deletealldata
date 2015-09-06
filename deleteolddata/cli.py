9c36be072151305a3d693d5ea83aaac314231611
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-09-06 13:18:45 +0900

    clean up a bit

--- deleteolddata/cli.py
+++ deleteolddata/cli.py
@@ -5,19 +5,29 @@ from delete import delete
 
 
 @click.command()
-@click.option('--older-than-days', default=10, help="Drop data older than this number of days")
-@click.option('--dataset-name', default=None, help="Only delete data for this dataset name. If this is not given, all datasets will be pruned of old data")
-@click.option('--dry-run', default=False, help="Don't actually do anything, just output the  directories that would have data deleted")
+@click.option('--older-than-days',
+              default=10,
+              help="Drop data older than this number of days")
+@click.option('--dataset-name', default=None,
+              help="Only delete data for this dataset name."
+              "If this is not given, all datasets will be pruned of old data")
+@click.option('--dry-run', default=False,
+              help="Don't actually do anything, just output the"
+              "directories that would have data deleted")
 @click.argument('path')
 def delete_all_data(older_than_days, dataset_name, dry_run, path):
 
-    dataset_string = " for dataset {0}".format(dataset_name) if dataset_name else ""
-    print "deleting records older than {0} days{1}".format(older_than_days, dataset_string)
+    ds_string = " for dataset {0}".format(dataset_name) if dataset_name else ""
+    message = "deleting records older than {0} days{1}".format(
+        older_than_days, ds_string)
+    print message
+
     if dry_run:
         print "Dry run - will not delete any files"
 
     today = datetime.datetime.today()
 
+    # Remove trailing slashes
     path = path.rstrip('/')
 
     delete(path, today, older_than_days, dataset_name, dry_run)
--- deleteolddata/delete.py
+++ deleteolddata/delete.py
@@ -4,6 +4,8 @@ import shutil
 
 
 def remove_directory(directory, dry_run):
+   # convenience which switches to simply printing
+   # directory names if dry run is specified
     print "deleting directory: {0}".format(directory)
     if dry_run:
         return
@@ -11,12 +13,16 @@ def remove_directory(directory, dry_run):
 
 
 def date_from_days(today, days):
+    # Convenience function for calculating the
+    # earliest possible date we want to keep
     delta_days = datetime.timedelta(days)
     min_date = today - delta_days
     return [min_date.year, min_date.month, min_date.day]
 
 
 def calculate_recursion_depth(base_root, current_root):
+    # Check what types of folders (e.g. datasets, years, months)
+    # the current dirictory contains
     base_root_length = len(base_root.split('/'))
     root_length = len(current_root.split('/'))
     recursion_depth = root_length - base_root_length
@@ -30,25 +36,39 @@ def calculate_recursion_depth(base_root, current_root):
 
 
 def process_directories(root, directories, filters, current_level, dry_run):
+    # There are no directories to process here so we're done
     if not len(directories):
         return
 
+    # We're past the longest relevant recursion depth (day) so just remove
+    # all entries from the `directories` list so that os.walk will skip them
     if current_level == 'skip':
         for i in range(len(directories)):
             directories.pop(0)
         return
 
+    # If we're at the data set level of recursion but no dataset
+    # has been specified, we can return as there's no need to filter
+    # out directoreis based on dataset
     if current_level == 'dataset'and not filters['dataset']:
         return
 
     if current_level == 'dataset':
-        # Need to use mutable sequence here to make in place change to list
+        # Remove entires from `directories` that dont match the user specified
+        # dataset. This will cause os.walk to skip processing them
+        #
+        # Note: Need to use mutable sequence here to make in place
+        # change to list
         # TODO (sokeefe): probably a nicer way to do this...
         for i in range(len(directories)):
             directories.pop(0)
         directories.append(filters['dataset'])
         return
 
+    # Get the minimum allowable value for this particular level.
+    # Directories with values lower can be deleted. Directories with
+    # higher values can be kept and hence removed from the processing queue.
+    # Directories with the same value will need to be recursed into.
     min_value = filters[current_level]
 
     for dir in [dir for dir in directories if int(dir) < min_value]:
@@ -59,6 +79,8 @@ def process_directories(root, directories, filters, current_level, dry_run):
         directories.remove(val)
 
 
+# For a specified directory remove all folders that are older than specified
+# number of days
 def delete(top_directory, today, older_than_days, dataset_name, dry_run):
 
     min_date = date_from_days(today, older_than_days)
@@ -69,6 +91,7 @@ def delete(top_directory, today, older_than_days, dataset_name, dry_run):
         'day':   min_date[2]
     }
 
+    # Iterate through each directory inside of root.
     for root, directories, files in os.walk(top_directory):
         current_level = calculate_recursion_depth(top_directory, root)
         process_directories(root, directories, filters, current_level, dry_run)
