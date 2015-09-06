c22c849249cf02aa746bbd59adcb437373bc0886
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-09-06 19:27:08 +0900

    add basic input checks

--- deleteolddata/cli.py
+++ deleteolddata/cli.py
@@ -1,12 +1,13 @@
 import click
 import datetime
+import os
 
 from delete import delete
 
 
 @click.command()
 @click.option('--older-than-days',
-              default=10,
+              default=10, type=click.IntRange(0, None),
               help="Drop data older than this number of days")
 @click.option('--dataset-name', default=None,
               help="Only delete data for this dataset name."
@@ -14,8 +15,12 @@ from delete import delete
 @click.option('--dry-run', default=False, type=bool, is_flag=True,
               help="Don't actually do anything, just output the"
               "directories that would have data deleted")
-@click.argument('path')
+@click.argument('path', type=click.Path(exists=True))
 def delete_old_data(older_than_days, dataset_name, dry_run, path):
+
+    if not os.path.isdir(os.path.join(path, dataset_name)):
+        print "{0} is not a valid dataset".format(dataset_name)
+        return 1
 
     ds_string = " for dataset {0}".format(dataset_name) if dataset_name else ""
     message = "deleting records older than {0} days{1}".format(
