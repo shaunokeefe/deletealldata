3e952bd3bfe4303212a238b9416e6da8fa6008e9
shaunokeefe <shaun.okeefe.0@gmail.com>
2015-09-06 11:49:44 +0900

    add helper function for processing each directory

--- deleteolddata/delete.py
+++ deleteolddata/delete.py
@@ -1,4 +1,9 @@
+import os
 import datetime
+
+
+def remove_directory(directory, dry_run):
+    pass
 
 
 def date_from_days(today, days):
@@ -20,9 +25,38 @@ def calculate_recursion_depth(base_root, current_root):
     return depth_to_field[recursion_depth]
 
 
-def process_dir(datasets, min_date, root, directory):
-    pass
+def process_directories(directories, filters, current_level, dry_run):
+    if not len(directories):
+        return
+
+    if current_level == 'dataset' and filters['dataset']:
+        # Need to use mutable sequence here to make in place change to list
+        # TODO (sokeefe): probably a nicer way to do this...
+        for i in range(len(directories)):
+            directories.pop(0)
+        directories.append(filters['dataset'])
+        return
+
+    min_value = filters[current_level]
+
+    for dir in [dir for dir in directories if int(dir) < min_value]:
+        remove_directory(dir, dry_run)
+
+    values_to_skip = [val for val in directories if int(val) != min_value]
+    for val in values_to_skip:
+        directories.remove(val)
 
 
-def delete(older_than_days, dataset_name, dry_run):
-    pass
+def delete(top_directory, older_than_days, dataset_name, dry_run):
+
+    min_date = date_from_days(older_than_days)
+    filters = {
+        'dataset': dataset_name,
+        'year': min_date.year,
+        'month': min_date.month,
+        'day': min_date.day,
+    }
+
+    for root, directories, files in os.walk():
+        current_level = calculate_recursion_depth(top_directory, root)
+        process_directories(directories, filters, current_level)
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import unittest
 import datetime
+import mock
+import unittest
 
-from deleteolddata.delete import date_from_days, calculate_recursion_depth
+from deleteolddata.delete import date_from_days, calculate_recursion_depth,\
+    process_directories
 
 
 class CalculateOlderTest(unittest.TestCase):
@@ -71,6 +73,82 @@ class CalculateCurrentLevelTest(unittest.TestCase):
         level = calculate_recursion_depth(root, current)
 
         self.assertEqual(level, 'day')
+
+
+class ProcessDirectoriesTest(unittest.TestCase):
+
+    def test_dataset(self):
+        dataset = 'dataset1'
+        filters = {
+            'dataset': dataset,
+            'year': 2015,
+            'month': 9,
+            'day': 1,
+        }
+        current_level = 'dataset'
+        directories = ['dataset1', 'dataset2', 'dataset3']
+        dry_run = False
+
+        process_directories(directories, filters, current_level, dry_run)
+
+        self.assertEqual(len(directories), 1)
+        self.assertEqual(directories[0], 'dataset1')
+
+    @mock.patch('deleteolddata.delete.remove_directory')
+    def test_year(self, remove_directory_mock):
+        dataset = None
+        filters = {
+            'dataset': dataset,
+            'year': 2014,
+            'month': 9,
+            'day': 1,
+        }
+        current_level = 'year'
+        directories = ['2013', '2014', '2015']
+        dry_run = False
+
+        process_directories(directories, filters, current_level, dry_run)
+
+        self.assertEqual(len(directories), 1)
+        self.assertTrue('2014' in directories)
+        remove_directory_mock.assert_has_calls([mock.call('2013', False)])
+
+    @mock.patch('deleteolddata.delete.remove_directory')
+    def test_month(self, remove_directory_mock):
+        dataset = None
+        filters = {
+            'dataset': dataset,
+            'year': 2014,
+            'month': 9,
+            'day': 1,
+        }
+        current_level = 'month'
+        directories = ['09', '08', '01']
+        dry_run = False
+
+        process_directories(directories, filters, current_level, dry_run)
+
+        self.assertEqual(len(directories), 1)
+        self.assertTrue('09' in directories)
+        remove_directory_mock.assert_has_calls([mock.call('08', False), mock.call('01', False)])
+
+    @mock.patch('deleteolddata.delete.remove_directory')
+    def test_no_directories_to_process(self, remove_directory_mock):
+        dataset = None
+        filters = {
+            'dataset': dataset,
+            'year': 2014,
+            'month': 9,
+            'day': 1,
+        }
+        current_level = 'day'
+        directories = []
+        dry_run = False
+
+        process_directories(directories, filters, current_level, dry_run)
+
+        self.assertEqual(len(directories), 0)
+        self.assertEqual(remove_directory_mock.call_count, 0)
 
 if __name__ == '__main__':
     unittest.main()
